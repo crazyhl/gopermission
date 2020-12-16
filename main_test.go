a35928a10a6640d16a53fbe07450b32fe038bb96
M1racle.Hao <crazyhl@163.com>
2020-12-16 18:07:23 +0800

    update test

--- main_test.go
+++ main_test.go
@@ -1,21 +1,13 @@
 package gopermission
 
 import (
-	"github.com/crazyhl/gopermission/route"
 	"testing"
 )
 
 func Test_Route_Parse(t *testing.T) {
-	var ctxParams [route.MaxParams]string
 	path := "/user/keys/:id/:name"
-	rp := GetParams(path)
-	t.Log(rp.Params)
 	url := "/user/keys/123/456"
-	result := rp.GetMatch(url, url, &ctxParams, false)
-	t.Log(result)
-	t.Log(ctxParams)
-	value := route.Params(rp.Params, ctxParams, "id")
-	t.Log(value)
-	value = route.Params(rp.Params, ctxParams, "name")
+	value := GetParams(path, url, "id")
+
 	t.Log(value)
 }
