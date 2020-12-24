e441ebdcf70017827857f7d0671b330e3d077c8a
M1racle.Hao <crazyhl@163.com>
2020-12-24 18:12:07 +0800

    优化字段约定

--- models/base_model.go
+++ models/base_model.go
@@ -3,5 +3,5 @@ package models
 type BaseModel struct {
 	ID        uint32 `gorm:"autoIncrement;primaryKey"`
 	CreatedAt uint32 `gorm:"autoCreateTime;` // 使用时间戳秒数填充创建时间
-	UpdatedAt uint32 `gorm:"autoCreateTime;` // 使用时间戳秒数填充创建时间
+	UpdatedAt uint32 `gorm:"autoUpdateTime;` // 使用时间戳秒数填充创建时间
 }
