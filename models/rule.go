3f130046572f69a30d2a616b5d443683c7e52960
M1racle.Hao <crazyhl@163.com>
2020-12-24 15:07:10 +0800

    增加字段

--- models/rule.go
+++ models/rule.go
@@ -10,6 +10,7 @@ import (
 type Rule struct {
 	BaseModel
 	Name        string       `gorm:"not null;default: '';uniqueIndex" validate:"required"` // 权限名称，必填，唯一
+	Description string       `gorm:"not null;default: '';"`                                // 描述
 	Permissions []Permission `gorm:"many2many:rule_permissions;"`
 }
 
